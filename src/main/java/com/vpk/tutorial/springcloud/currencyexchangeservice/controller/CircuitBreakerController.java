7bd2196369578a178172ca7a24e92515132a097f
VP Keerthan <keerthanvp@gmail.com>
2021-06-20 13:11:03 +0530

    Adding Circuit Breaker and Rate Limiter

--- src/main/java/com/vpk/tutorial/springcloud/currencyexchangeservice/controller/CircuitBreakerController.java
+++ src/main/java/com/vpk/tutorial/springcloud/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,5 +1,7 @@
 package com.vpk.tutorial.springcloud.currencyexchangeservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,11 +20,37 @@ public class CircuitBreakerController {
     public String sampleAPI(){
         logger.info("Sample API call received");
         ResponseEntity<String> responseEntity = new RestTemplate()
+                .getForEntity("http://localhost:8000/sample-url",String.class);
+        return responseEntity.getBody();
+    }
+
+    @GetMapping("/dummy-api")
+    @CircuitBreaker(name = "dummy-api", fallbackMethod = "dummyAPIFallback")
+    public String dummyAPI(){
+        logger.info("Dummy API call received");
+        ResponseEntity<String> responseEntity = new RestTemplate()
                 .getForEntity("http://localhost:8000/dummy-url",String.class);
+        return responseEntity.getBody();
+    }
+
+    @GetMapping("/dummy-api-v2")
+    @RateLimiter(name = "dummy-api-v2", fallbackMethod = "dummyAPIFallbackV2")
+    public String dummyAPIV2(){
+        logger.info("Dummy API-V2 call received");
+        ResponseEntity<String> responseEntity = new RestTemplate()
+                .getForEntity("http://localhost:8000/dummy-url-v2",String.class);
         return responseEntity.getBody();
     }
 
     private String sampleAPIFallback(Exception e){
         return "Fallback response for Sample API";
+    }
+
+    private String dummyAPIFallback(Exception e){
+        return "Fallback response for Dummy API";
+    }
+
+    private String dummyAPIFallbackV2(Exception e){
+        return "Fallback response for Dummy API-V2";
     }
 }
